e7f43f0133005b54597b72fa85cea8beb2de4af6
Vova <vladimir.bezhenar@gmail.com>
2016-04-10 12:03:33 +0300

    Update tasks

--- JavaRush/src/com/javarush/test/level00/lesson02/task01/Task.java
+++ JavaRush/src/com/javarush/test/level00/lesson02/task01/Task.java
@@ -5,11 +5,10 @@ package com.javarush.test.level00.lesson02.task01;
 Пример вывода на экран:
 Я думаю, быть программистом - это круто
 */
-public class Solution
+public class Task
 {
     public static void main(String[] args)
     {
         //напишите тут ваш код
-        System.out.println("Я думаю, быть программистом - это круто");
     }
 }
