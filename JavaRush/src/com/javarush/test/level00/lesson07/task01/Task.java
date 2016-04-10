6f72f78ed73f55b9303ec93b0e0b91e02feb34e3
Vova <vladimir.bezhenar@gmail.com>
2016-04-10 12:25:38 +0300

    yo

---
+++ JavaRush/src/com/javarush/test/level00/lesson07/task01/Task.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level00.lesson07.task01;
+
+/* Это не баг, это фича.
+Напиши программу, которая выводит на экран надпись «Это не баг, это фича.».
+Пример вывода на экран:
+Это не баг, это фича.
+*/
+public class Task
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+    }
+}
