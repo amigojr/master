f9ff8d835a6f853ae8e0c6e287013d218977eb4e
Vova <vladimir.bezhenar@gmail.com>
2016-04-10 11:58:33 +0300

    solution for task01

--- JavaRush/src/com/javarush/test/level00/lesson02/task01/Solution.java
+++ JavaRush/src/com/javarush/test/level00/lesson02/task01/Solution.java
@@ -10,5 +10,6 @@ public class Solution
     public static void main(String[] args)
     {
         //напишите тут ваш код
+        System.out.println("Я думаю, быть программистом - это круто");
     }
 }
